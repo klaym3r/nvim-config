43afa95dd01fb893eef94b28e42c8201be6f09fd
klaym3r <143213580+klaym3r@users.noreply.github.com>
2024-09-11 12:23:54 +0300

    Add files via upload

---
+++ nvim/lua/community.lua
@@ -0,0 +1,13 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.colorscheme.nordic-nvim" },
+  { import = "astrocommunity.colorscheme.everforest" },
+  -- import/override with your plugins folder
+}
